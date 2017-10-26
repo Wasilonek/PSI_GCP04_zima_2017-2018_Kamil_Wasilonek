b7521009deddfffbbbbc1eb2872b5aae2000082a
Pezet <wasilonek@interia.eu>
2017-10-26 21:25:32 +0200

    1.1

--- PerceptronProject/src/Main.java
+++ PerceptronProject/src/Main.java
@@ -1,68 +1,65 @@
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        //Tworzenie perceptronu z nadaniem żądanej ilości wejść
         Perceptron perceptron = new Perceptron(3);
 
-        int howManyStepsToLearn = 10;
+        //Podanie ilości epok i kroku uczenia
+        int howManyStepsToLearn = 0;
         double learn_rate = 0.1;
 
+        //Podanie danych uczących
         int bias = 1;
-        int[] input1 = { 0, 1, 0, 1 };
-        int[] input2 = { 0, 1, 1, 0 };
+        int[] input1 = {0, 1, 0, 1};
+        int[] input2 = {0, 1, 1, 0};
 
+        //Podanie oczekiwanych wartości dla poszczególnych zestawów uczących
         int[] expectedData = {0, 1, 0, 0}; //AND
+        int[] outputData = new int[4];
 
-        for (int i = 0; i < howManyStepsToLearn; i++) {
-            for (int j = 0; j < input1.length; j++) {
-                int[] inputData = {bias,input1[j],input2[j]};
-                perceptron.learnFunction( inputData , expectedData[j], learn_rate);
+
+//        Pętla realizująca uczenie perceptronu założoną ilość epok
+//        for (int i = 0; i < howManyStepsToLearn; i++) {
+//            for (int j = 0; j < input1.length; j++) {
+//                int[] inputData = {bias,input1[j],input2[j]};
+//                perceptron.learnFunction( inputData , expectedData[j], learn_rate);
+//            }
+//        }
+
+
+        // pętla sprawdzająca zgodnosc wartości wyliczonych z oczekiwanymi
+        while (!Arrays.equals(expectedData, outputData)) {
+
+            for (int i = 0; i < 4; i++) {
+                int[] inputData = {bias, input1[i], input2[i]};
+                perceptron.learnFunction(inputData, expectedData[i], learn_rate);
             }
+
+            for (int i = 0; i < 4; i++) {
+                outputData[i] = perceptron.calculateOutput(new int[]{bias, input1[i], input2[i]});
+            }
+
+            howManyStepsToLearn++;
+
+            if (howManyStepsToLearn > 1000) break;
         }
 
-
-//        perceptron.learnFunction( new int[]{1,0,0} , 0, learn_rate);
-//        perceptron.learnFunction( new int[]{1,0,0} , 0, learn_rate);
-//        perceptron.learnFunction( new int[]{1,0,0} , 0, learn_rate);
-//        perceptron.learnFunction( new int[]{1,0,0} , 0, learn_rate);
-//        perceptron.learnFunction( new int[]{1,0,0} , 0, learn_rate);
-//
-//        perceptron.learnFunction( new int[]{1,1,1} , 1, learn_rate);
-//        perceptron.learnFunction( new int[]{1,1,1} , 1, learn_rate);
-//
-//        perceptron.learnFunction( new int[]{1,0,1} , 0, learn_rate);
-//        perceptron.learnFunction( new int[]{1,0,1} , 0, learn_rate);
-//        perceptron.learnFunction( new int[]{1,0,1} , 0, learn_rate);
-//        perceptron.learnFunction( new int[]{1,0,1} , 0, learn_rate);
-//        perceptron.learnFunction( new int[]{1,0,1} , 0, learn_rate);
-//        perceptron.learnFunction( new int[]{1,0,1} , 0, learn_rate);
-//
-//        perceptron.learnFunction( new int[]{1,1,0} , 0, learn_rate);
-//        perceptron.learnFunction( new int[]{1,1,0} , 0, learn_rate);
-//        perceptron.learnFunction( new int[]{1,1,0} , 0, learn_rate);
-
-
-
-
-
-
-
-
+        // wypisanie końcowych wag
         double weight[] = perceptron.getWeights();
-        for(int i = 0 ; i < weight.length ; i++ ){
-            System.out.println("weight: " + weight[i]);
+        for (int i = 0; i < weight.length; i++) {
+            System.out.println("\nEnd weight: " + weight[i]);
         }
 
-        System.out.println();
+        // wypisanie końcowego wyliczonego wyniku
+        for (int i = 0; i < outputData.length; i++) {
+            System.out.println(outputData[i]);
+        }
 
-        System.out.println("Expected output:" + expectedData[0]);
-        System.out.println("\tCalculated output:" + perceptron.calculateOutput(new int[] {bias, 0, 0}));
-        System.out.println("Expected output:" + expectedData[1]);
-        System.out.println("\tCalculated output:" + perceptron.calculateOutput(new int[] {bias, 1, 1}));
-        System.out.println("Expected output:" + expectedData[2]);
-        System.out.println("\tCalculated output:" + perceptron.calculateOutput(new int[] {bias, 0, 1}));
-        System.out.println("Expected output:" + expectedData[3]);
-        System.out.println("\tCalculated output:" + perceptron.calculateOutput(new int[] {bias, 1, 0}));
+        System.out.println("\nIle kroków potrzebnych do nauki: " + howManyStepsToLearn);
+
     }
 }
